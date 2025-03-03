1ab8040088f6fdfc6039c25b304f1dbebb14a654
TimeJerker <krov1chh@gmail.com>
2025-03-03 11:51:03 +0400

    добавление проверки на переполнение и деление на ноль. добавление комментариев

--- Lab1-2/Lab1-2/lab1-2.cpp
+++ Lab1-2/Lab1-2/lab1-2.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <windows.h>
 #include <tchar.h>
+#include <iostream>
+
+using namespace std;
+
 void main()
 {
     char s[20];
     int a, b, c, sum;
+    int error_zero = 0, error_over = 0;
     CharToOem(_T("Введите "), s);
     printf("%s a: ", s);
     scanf_s("%d", &a);
@@ -17,28 +22,55 @@ void main()
         mov eax, a;
         mov ebx, b;
         mov ecx, 4;
-    m: add edx, c
+    m: add edx, c //<edx>=4*c
         loop m
-        sub edx, ebx;
-        inc edx;
+        sub edx, ebx; //<edx> = 4*<edx>-<ebx> = 4*c-b
+        inc edx; //<edx> = 4*c-b+1
 
-        push edx;
-        imul ebx, eax;
+        push edx; //в стеке 4*с-b+1
+        imul ebx, eax; //<ebx> = b * a
+        jo error_of;
 
-        mov ecx, 2;
-        mov eax, c;
+        mov ecx, 2; //<ecx> = 2
+        mov eax, c; //<eax> = c
         cdq;
-        idiv ecx; //т.к для деления используется edx:eax
+        idiv ecx; //<eax> = c/2
 
-        add eax, ebx;
+        add eax, ebx; //<eax> = c/2 + b*a
+        jo error_of;
 
-        pop ebx;
+        pop ebx; //достаем из стека 4*c-b+1
+        test ebx, ebx;
+        jz error_zf;
         cdq;
-        idiv ebx;
+        idiv ebx; //<aex> = (c/2 + b*a)/(4*c-b+1)
+
         mov sum, eax;
+        jmp end_asm;
+
+    error_of:
+        mov sum, 0; //обнуляем результат
+        mov error_over, -1;
+        jmp end_asm;
+
+    error_zf:
+        mov sum, 0; //обнуляем результат
+        mov error_zero, -1;
+        jmp end_asm;
+
+    end_asm:
+        ;
     }
     //(b*a+c/2)/(4*c-b+1)
     //printf("sum: %d", sum);
-    printf("\n (%d * %d + %d/2)/(4 * %d - %d + 1)= %d", b, a, c, c, b, sum);
+    if (error_zero != 0) {
+        cout << "Division by zero" << endl;
+    }
+    else if (error_over != 0) {
+        cout << "Overflow" << endl;
+    }
+    else {
+        printf("\n (%d * %d + %d/2)/(4 * %d - %d + 1)= %d", b, a, c, c, b, sum);
+    }
     getchar(); getchar();
 }
